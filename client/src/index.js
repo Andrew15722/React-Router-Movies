1e4af52ed0c56a49d9a785371a9ae58bd5f640ae
andrew-mendez <andrew15722@yahoo.com>
2019-07-23 14:13:27 -0700

    met mvp goals, wrapped the movie details component in a link component and grabbed the id info from the useEffect and created a home route and dynamic moive id route.

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+// Wrap your app with the router.
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('root')
+);
